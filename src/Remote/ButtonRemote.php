e8f2ea202efed1467af40485f5877c6fbd05b8e9
Kevin Bond <kevinbond@gmail.com>
2024-08-23 16:24:43 -0400

    Step 10: AutowireIterator: indexAttribute

--- src/Remote/ButtonRemote.php
+++ src/Remote/ButtonRemote.php
@@ -8,7 +8,7 @@ use Symfony\Component\DependencyInjection\Attribute\AutowireIterator;
 final class ButtonRemote
 {
     public function __construct(
-        #[AutowireIterator(ButtonInterface::class)]
+        #[AutowireIterator(ButtonInterface::class, indexAttribute: 'key')]
         private iterable $buttons,
     ) {
     }
