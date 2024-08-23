5bd08cc2d00a4330483c6cbbf9296c2f8651ff1f
Kevin Bond <kevinbond@gmail.com>
2024-08-23 16:24:47 -0400

    Step 33: Exclude: add

--- src/Remote/Button/DiagnosticsButton.php
+++ src/Remote/Button/DiagnosticsButton.php
@@ -3,10 +3,12 @@
 namespace App\Remote\Button;
 
 use Symfony\Component\DependencyInjection\Attribute\AsTaggedItem;
+use Symfony\Component\DependencyInjection\Attribute\Exclude;
 use Symfony\Component\DependencyInjection\Attribute\When;
 
 #[AsTaggedItem('diagnostics')]
 #[When('dev')]
+#[Exclude]
 final class DiagnosticsButton implements ButtonInterface
 {
     public function press(): void
